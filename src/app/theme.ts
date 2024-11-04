287bc3fe8957aa7dd0ccb199269c73f6b8d0ec84
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-11-03 18:36:17 -0800

    Update theme colors

---
+++ src/app/theme.ts
@@ -0,0 +1,37 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1ed760', // Orange - main brand color
+      highlight: "#3be477",
+      press: "#1abc54"
+    },
+    secondary: {
+      main: '#333333', // Dark grey
+      light: '#4F4F4F', // Lighter shade for contrast
+      dark: '#1A1A1A', // Darker shade
+      contrastText: '#FFFFFF', // White text on secondary color
+    },
+    background: {
+      default: '#121212', // Light grey background
+      highlight: '#1f1f1f',   // Paper background for cards, etc.
+      elevatedPress: "#191919"
+    },
+    text: {
+      primary: '#333333', // Dark grey for text
+      secondary: '#757575', // Medium grey for secondary text
+    },
+  },
+  typography: {
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    button: {
+      textTransform: 'none',
+    },
+  },
+});
+
+export default theme;
+
