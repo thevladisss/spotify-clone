0b04fd0cfb5d2eed22f76c4a2b7e54a9e7853a6b
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-11-08 21:42:42 -0800

    Update UI and add input handling logic for HomeSearchInput component

--- src/app/components/HomeSearchInput.tsx
+++ src/app/components/HomeSearchInput.tsx
@@ -1,40 +1,61 @@
 'use client'
-import React, {useState} from 'react'
+import React, {FormEvent, SyntheticEvent, useMemo, useState} from 'react'
 import AppInput from "@/app/components/app/AppInput";
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
+import BrowserUpdatedIcon from '@mui/icons-material/BrowserUpdated';
 import { IconButton } from '@mui/material';
+import styles from "@/app/css/HomeSearchInput.module.css"
 function HomeSearchInput(props: any): JSX.Element {
 
-  const [value ,setValue] = useState()
-  
+  const [input, setInput] = useState("")
+
+  const handleInput = (event: FormEvent<HTMLInputElement>) => {
+
+    if (event && event.target) {
+      setInput(event.target.value)
+    }
+  }
+
+  const handleClearInput = () => {
+    setInput("")
+  }
+
+  const hasInput = useMemo(() => Boolean(input.length),[input]);
+
   const searchElementIcon = <div>
-    <IconButton size="medium" color="white" style={{paddingLeft: 0}}>
+    <IconButton className={styles.SearchIcon} disableRipple size="medium" color="white">
       <SearchIcon></SearchIcon>
     </IconButton>
   </div>
 
   const closeElementIcon = <div>
-    <IconButton onClick={() => {
-      debugger
-    }} size="medium" color="white" style={{paddingLeft: 0}}>
+    <IconButton onClick={handleClearInput} className={styles.CloseIcon} disableRipple size="large" color="white">
       <CloseIcon></CloseIcon>
     </IconButton>
   </div>
 
-  const [isFocused, setFocused] = useState(false)
+  const browseElementIcon = <div>
+    <IconButton className={styles.BrowseIcon} disableRipple size="small" color="white">
+      <BrowserUpdatedIcon></BrowserUpdatedIcon>
+    </IconButton>
+  </div>
+
+  const endAdornment = hasInput ? closeElementIcon : browseElementIcon;
 
   return (
     <AppInput
-      onFocus={() => {setFocused(true)}}
-      onBlur={() => {setFocused(false)}}
+      onInput={handleInput}
       size="small"
       style={{width: "400px"}}
       color="white"
+      placeholder="What do you want to play?"
+      className={styles.HomeSearchInput}
+      value={input}
       slotProps={{
         input: {
           startAdornment: searchElementIcon,
-          endAdornment: isFocused ? closeElementIcon : <></>,
+          endAdornment: endAdornment
         }
       }}
     ></AppInput>
