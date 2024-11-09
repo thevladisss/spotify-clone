4ec25ebeaef0497472571a70eb3c828bf641e3a9
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-11-09 11:18:03 -0800

    Update icons in Navigation bar

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, {useState} from 'react'
-import AppInput from '@/app/components/app/AppInput'
-import { Button, TextField} from "@mui/material";
+import AppButtonFab from "@/app/components/app/AppButtonFab";
+import HomeIcon from "@/app/components/app/HomeIcon";
 import AppButton from "@/app/components/app/AppButton";
 import HomeSearchInput from "@/app/components/HomeSearchInput";
 import Image from 'next/image'
@@ -12,11 +12,17 @@ function Navigation(): JSX.Element {
     <div className="p-2 flex justify-between relative w-full bg-black">
         <div className="flex gap-8 items-center justify-between w-4/6 w-full">
           <div className="columns-auto">
-            <Image height={32} width={32} src="/icons/logo_spotify_white.png" alt="Logo" />
+            <Image height={24} width={24} src="/icons/logo_spotify_white.png" alt="Logo" />
           </div>
-            <HomeSearchInput></HomeSearchInput>
+            <div className="flex gap-2">
+              {/*TODO: Do not use styles for at least border. AppButton should be customizable*/}
+              <AppButtonFab disableRipple disableTouchRipple disableFocusRipple color="white" sx={{backgroundColor: "background.highlight", height: "48px", width: "48px"}}>
+                <HomeIcon></HomeIcon>
+              </AppButtonFab>
+              <HomeSearchInput></HomeSearchInput>
+            </div>
           <div className="columns-auto flex col-2">
-            <AppButton textColor="rgb(179, 179, 179)" className="mb-1" transparent>
+            <AppButton textColor="rgb(179, 179, 179)" className="mb-1" size="large" transparent>
               Sign up
             </AppButton>
             <AppButton color="white" textColor="black" className="py-1 mb-1" size="large">
