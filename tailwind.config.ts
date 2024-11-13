978c2ab9423f98605ee30e5ca44ae590b3c5c8d5
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-11-12 20:40:42 -0800

    Add CSS variables for breakpoints. Customize breakpoints in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': "650px",
+      'md': "840px",
+      'lg': "1400px",
+      'xl': "1920px",
+    },
+
     extend: {
       colors: {
         background: "var(--background)",
