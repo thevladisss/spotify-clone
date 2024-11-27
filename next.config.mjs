5cd3732417000759bd778b6cb97f75dfc439b756
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-11-26 18:08:48 -0800

    Update Next config to tolerate images from all domains

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
   }
 };
 
